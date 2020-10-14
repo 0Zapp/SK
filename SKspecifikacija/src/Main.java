af7528263c33148ee9d069dbcda97c4c5c0f2f5f
Marko Nedeljkovic <marko65734@gmail.com>
2020-10-14 07:32:48 +0200

    Figured out how to generate documentation

--- SKspecifikacija/src/Main.java
+++
@@ -1,8 +0,0 @@
-import DBHandler.DBHandler;
-
-public class Main {
-	public static void main(String[] args) {
-		DBHandler dbh = new DBHandler();
-		dbh.loadData("C:\\Users\\rektix\\Desktop\\faks\\SK");
-	}
-}
