559877a650b77ad59be8ff8b9f2e0fe76c2ec630
Marko Nedeljkovic <marko65734@gmail.com>
2020-10-19 17:27:01 +0200

    Json succesfully implemented

--- data-repository-json/src/main/java/main/Main.java
+++ data-repository-json/src/main/java/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import jsonHandler.JsonHandler;
@@ -9,10 +10,20 @@ import model.Entity;
 public class Main {
 
 	public static void main(String[] args) {
-		JsonHandler jh = new JsonHandler("test.json", 10);
-		List<Entity> data = jh.load("test.json");
+		JsonHandler yh = new JsonHandler("sdfsdfd", 10);
+		List<Entity> data = new ArrayList<Entity>();
+		Entity e = new Entity();
+		e.setName("test");
+		e.setId("123");
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("abc", "def");
+		map.put("zxv", 123);
+		map.put("wpf", true);
+		e.setData(map);
+		data.add(e);
+		yh.dump("test.json", data);
+		data=yh.load("test.json");
 		System.out.println(data);
-
 	}
 
 }
